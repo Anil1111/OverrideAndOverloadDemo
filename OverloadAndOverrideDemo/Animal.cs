cbe26aace23882a1f69bb69868af04e79ca3ad7a
Steven Williams <steven.williams12083@hotmail.com>
2014-03-16 15:46:09 -0500

    Initial Commit

---
+++ OverloadAndOverrideDemo/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadAndOverrideDemo
+{
+    abstract class Animal
+    {
+        public virtual void MakeSound() 
+        { 
+            Console.WriteLine("default sound."); 
+        }
+
+    }
+}
---
+++ OverloadAndOverrideDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadAndOverrideDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newDog = new Dog();
+            var newCat = new Cat();
+            var newDuck = new Duck();
+
+            newDog.MakeSound();
+            newCat.MakeSound();
+            newDuck.MakeSound();
+
+            Console.ReadLine();
+        }
+    }
+}
